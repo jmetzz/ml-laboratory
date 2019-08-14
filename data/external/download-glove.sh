32c1b7471f1821d9a46c2b4a7a0f5a84a840f53d
Jean Metz <jmetzz@gmail.com>
2019-08-14 19:17:05 +0200

    Add some examples using keras

---
+++ data/external/download-glove.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget http://nlp.stanford.edu/data/glove.6B.zip -O ../non_versioned/globe.6B.zip
+cd ../non_versioned
+unzip glove.6B.zip
--- src/main/python/utils/data_helper.py
+++ src/main/python/utils/data_helper.py
@@ -1,5 +1,6 @@
 import csv
 import gzip
+import json
 import pickle
 import random
 import os
@@ -7,6 +8,7 @@ from typing import Tuple
 
 import numpy as np
 import h5py
+from keras.datasets import imdb
 
 
 def load_train_test_datasets(filename, split=0.5):
@@ -82,6 +84,73 @@ toy_unlabeled_dataset = [[2.771244718, 1.784783929],
                          [7.444542326, 0.476683375],
                          [10.12493903, 3.234550982],
                          [6.642287351, 3.319983761]]
+
+
+def load_imdb_pickle(vocabulary_size=1000):
+    """Loads the keras imdb dataset
+
+    Since numpy version 1.16.4 sets allow_pickle to False by default,
+    we need to overwrite this parameter to be able to load the dataset into memory.
+
+    We should, obviously, reset the default parameters later.
+    """
+
+    # save np.load
+    np_load_old = np.load
+
+    # modify the default parameters of np.load
+    np.load = lambda *a, **k: np_load_old(*a, allow_pickle=True, **k)
+
+    # call load_data with allow_pickle implicitly set to true
+    (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=vocabulary_size)
+
+    # restore np.load for future normal usage
+    np.load = np_load_old
+    return (X_train, y_train), (X_test, y_test)
+
+
+def load_amazon():
+    filename = 'reviews_Video_Games_5.json'
+    train_summary = []
+    train_review_text = []
+    train_labels = []
+
+    test_summary = []
+    test_review_text = []
+    test_labels = []
+
+    with open(filename, 'r') as f:
+        for (i, line) in enumerate(f):
+            data = json.loads(line)
+
+            if data['overall'] == 3:
+                next
+            elif data['overall'] == 4 or data['overall'] == 5:
+                label = 1
+            elif data['overall'] == 1 or data['overall'] == 2:
+                label = 0
+            else:
+                raise Exception("Unexpected value " + str(data['overall']))
+
+            summary = data['summary']
+            review_text = data['reviewText']
+
+            if i % 10 == 0:
+                test_summary.append(summary)
+                test_review_text.append(review_text)
+                test_labels.append(label)
+            else:
+                train_summary.append(summary)
+                train_review_text.append(review_text)
+                train_labels.append(label)
+
+    return (train_summary, train_review_text, train_labels), (test_summary, test_review_text, test_labels)
+
+
+def load_amazon_smaller(size=20000):
+    (train_summary, train_review_text, train_labels), (test_summary, test_review_text, test_labels) = load_amazon()
+    return (train_summary[:size], train_review_text[:size], np.array(train_labels[:size])), (
+        test_summary[:size], test_review_text[:size], np.array(test_labels[:size]))
 
 
 class MNISTLoader:
