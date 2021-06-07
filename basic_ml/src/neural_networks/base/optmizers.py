cd3490e377a0ea4354a7564ffe9e979d2c9277ae
Jean Metz <4632809+username@users.noreply.github.com>
2021-06-07 17:41:31 +0200

    Make mono repo

---
+++ basic_ml/src/neural_networks/base/optmizers.py
@@ -0,0 +1,18 @@
+class GradientDescent:
+    def run(epochs, los, learning_rate=0.01):
+        """Find the weights wk and biases bl which minimize the cost function.
+
+
+        The update rule is:
+            \begin{eqnarray}
+              w_k & \rightarrow & w_k' = w_k-\eta \frac{\partial C}{\partial w_k} \\
+              b_l & \rightarrow & b_l' = b_l-\eta \frac{\partial C}{\partial b_l}.
+            \end{eqnarray}
+
+        """
+        raise NotImplementedError()
+
+
+class StochasticGradientDescent:
+    def evaluate(self, test_data):
+        raise NotImplementedError()
---
+++ basic_ml/src/neural_networks/examples/logistic_regression/cat_classifier.py
@@ -0,0 +1,38 @@
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from scipy import ndimage
+
+import numpy as np
+from utils import data_helper
+
+if __name__ == "__main__":
+
+    dataset_path = r"../data/raw/cat"
+    train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = data_helper.load_from_h5(
+        dataset_path, "catvnoncat"
+    )
+
+    m_train = train_set_x_orig.shape[0]
+    m_test = test_set_x_orig.shape[0]
+    num_px = train_set_x_orig.shape[1]
+
+    # Reshape the training and test examples
+    train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+    # standardize our dataset.
+    # One common preprocessing step in machine learning is to center and standardize your dataset,
+    # meaning that you substract the mean of the whole numpy array from each example, and then divide
+    # each example by the standard deviation of the whole numpy array. But for picture datasets,
+    # it is simpler and more convenient and works almost as well to just divide every row of the dataset
+    # by 255 (the maximum value of a pixel channel).
+    train_set_x = train_set_x_flatten / 255.0
+    test_set_x = test_set_x_flatten / 255.0
+
+    # To design a simple algorithm to distinguish cat images from non-cat images, base on Logistic Regression,
+    # perform the following steps:
+    # - Initialize the parameters of the model
+    # - Learn the parameters for the model by minimizing the cost
+    # - Use the learned parameters to make predictions (on the test set)
+    # - Analyse the results and conclude
