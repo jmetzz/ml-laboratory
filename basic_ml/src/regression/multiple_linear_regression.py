76b8e532fd4dc069e387841801fddd12e4b38821
Jean Metz <4632809+username@users.noreply.github.com>
2021-07-11 13:32:44 +0200

    Add regression examples

---
+++ basic_ml/src/regression/multiple_linear_regression.py
@@ -0,0 +1,136 @@
+from math import sqrt
+
+import numpy as np
+from numpy.linalg import inv
+from numpy import dot, power
+from numpy.random import default_rng
+
+
+def multiple_linear_regression_closed_form(observations, y_values):
+    """
+    This operation is very expensive. Calculating the inverse is N^3.
+    There are better solutions for this problem.
+
+    Notes:
+    - Num of observations must be greater than the number of features
+    - linearly independent
+    - w and y are vectors
+
+    RSS(w) = -2H^T (y - Hw)
+
+    Solve for w:
+        RSS(w_hat) = 0
+        -2H^T (y - H * w_hat) = 0
+        -2H^T * y + 2H^T * H * w_hat = 0
+        H^T * H * w_hat = H^T * y
+        inv(H^T * H) * H^T * H * w_hat = H^T * y * inv(H^T * H)
+
+        # inv(H^T * H) * H^T * H --> I
+        # thus:
+
+        I * w_hat = H^T * y * inv(H^T * H)
+        w_hat = inv(H^T * H) * H^T * y
+    """
+    return dot(
+        inv(observations.T @ observations),
+        observations.T.dot(y_values)
+    )
+
+
+def random_init(size, seed=12345678903141592653589793):
+    rng = default_rng(seed)
+    return rng.standard_normal(size)
+
+
+def gradient_descent_loop_form(feature_matrix,
+                               y_values,
+                               step_size: float = 0.01,
+                               tolerance: float = 0.05,
+                               max_iter: int = 100,
+                               initializer=random_init):
+    weights = initializer(feature_matrix.shape[1])
+    converged = False
+    iteration = 0
+    while not converged and iteration < max_iter:
+        # compute the predictions based on feature_matrix and weights using your predict_output() function
+        predictions = predict_output(feature_matrix, weights)
+        # compute the errors as predictions - output
+        residual_errors = predictions - y_values
+        gradient_sum_squares = 0  # initialize the gradient sum of squares
+        # while we haven't reached the tolerance yet, update each feature's weight
+        for i in range(len(weights)):  # loop over each weight
+            # Recall that feature_matrix[:, i] is the feature column associated with weights[i]
+            # compute the derivative for weight[i]:
+            derivative = feature_derivative(residual_errors, feature_matrix[:, i])
+            # add the squared value of the derivative to the gradient sum of squares (for assessing convergence)
+            gradient_sum_squares += derivative**2
+            # subtract the step size times the derivative from the current weight
+            weights[i] -= step_size * derivative
+        # compute the square-root of the gradient sum of squares to get the gradient magnitude:
+        gradient_magnitude = sqrt(gradient_sum_squares)
+        converged = gradient_magnitude < tolerance
+        iteration += 1
+    return weights
+
+
+def predict_output(feature_matrix: np.ndarray, weights: np.ndarray) -> np.ndarray:
+    """
+    Predict the regression value for N input elements
+
+    Args:
+        feature_matrix: a numpy matrix containing the features as columns
+        weights: the model weights as a numpy array
+
+    Returns:
+        the predictions vector as a np.array
+    """
+    return np.dot(feature_matrix, weights.T)
+
+
+def feature_derivative(feature, errors):
+    return 2 * np.dot(errors, feature)
+
+
+def gradient_descent_vectorized_form(feature_matrix,
+                                     y_values,
+                                     step_size: float = 0.01,
+                                     tolerance: float = 0.05,
+                                     max_iter: int = 100,
+                                     initializer=random_init):
+    weights = initializer(feature_matrix.shape[1])
+    converged = False
+    iteration = 0
+
+    while not converged and iteration < max_iter:
+        # prediction = predict_output(feature_matrix, weights)
+        # residual_errors = y_values - feature_matrix.T.dot(weights)
+        residual_errors = y_values - feature_matrix.dot(weights.T)
+        partials = -2 * feature_matrix.T.dot(residual_errors)
+        weights += step_size * partials
+        gradient_magnitude = np.sqrt(power(partials, 2).sum())
+        converged = gradient_magnitude < tolerance
+        iteration += 1
+    return weights
+
+
+if __name__ == "__main__":
+    # first feature is the constant 1 (intercept)
+    feature_matrix = np.array([[1, 10, 3],
+                               [1, 2.5, 7],
+                               [1, 15, 8],
+                               [1, 6, 2],
+                               [1, 67, 37],
+                               ])
+
+    y_values = np.array([-1, 0.2, 0.9, 3, 0.6])
+    step_size = 7e-12
+    tolerance = 2.5e7
+
+    closed_w = multiple_linear_regression_closed_form(feature_matrix, y_values)
+    loop_w = gradient_descent_loop_form(feature_matrix, y_values, step_size, tolerance)
+    vectorized_w = gradient_descent_vectorized_form(feature_matrix, y_values, step_size, tolerance)
+
+    print(closed_w)
+    print(loop_w)
+    print(vectorized_w)
+
---
+++ basic_ml/src/regression/simple_linear_regression.py
@@ -0,0 +1,143 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+from visualization.linear_plots import plot_line
+
+
+def ols_sklearn(feature, y_values):
+    """
+    Solve Ordinary Least Squares using scikit-learn's LinearRegression
+    """
+    estimator = LinearRegression()
+    # note that the intercept is built inside LinearRegression
+    estimator.fit(feature.reshape(-1, 1), y_values)
+    slope = estimator.coef_[0]
+    intercept = estimator.intercept_
+    return slope, intercept
+
+
+def ols_numpy(feature, y_values):
+    """
+    Solve Ordinary Least Squares using numpy.linalg.lstsq
+
+    Fits a line `y = mx + c`.
+    We can rewrite the line equation as `y = Ap`,
+    where A = [[x 1]] and p = [[m], [c]].
+
+    """
+    ones = np.ones(y_values.shape[0])
+    matrix = np.vstack((feature, ones)).T
+    slope, intercept = np.linalg.lstsq(matrix, y_values, rcond=None)[0]
+    return slope, intercept
+
+
+def ols_closed_form(feature, y_values):
+    """
+    Compute the closed form of linear regression
+
+    slope = (A - B) / (C - D)
+    intercept = [ sum(output) / N ] - slope * [ sum(feature) / N ]
+
+    where,
+        A = sum(feature * output)
+        B = [sum(feature) * sum(output)] * 1/N
+        C = sum(feature^2)
+        D = sum(feature)^2 * 1/N
+
+    Returns: the intercept and slope values as a tuple
+
+   """
+    size = feature.shape[0]
+    sum_features = np.sum(feature)
+    sum_squared_features = np.sum(feature * feature)
+    sum_output = np.sum(y_values)
+
+    # calculate slope
+    numerator = np.sum(feature * y_values) - (sum_features * sum_output) / size
+    denominator = sum_squared_features - (sum_features * sum_features) / size
+    slope = numerator / denominator
+
+    # use this computed slope to compute the intercept:
+    intercept = np.mean(y_values) - slope * np.mean(feature)
+    return slope, intercept
+
+
+def regression_prediction(input_feature, intercept, slope):
+    """
+    Calculate the predicted values based on the liner regression model
+
+    Returns:
+        the estimated value
+    """
+    return intercept + slope * input_feature
+
+
+def get_residual_sum_of_squares(input_feature, y_values, intercept, slope):
+    """
+    Calculate the Residual Sum of Squares (RSS)
+
+    The sum of the squares of the residuals and the residuals is
+    the difference between the predicted output and the true output.
+
+    Returns:
+        the RSS value
+    """
+
+    # First get the predictions
+    predictions = regression_prediction(input_feature, intercept, slope)
+    # then compute the residuals (since we are squaring it doesn't matter which order you subtract)
+    residuals = predictions - y_values
+    # square the residuals and add them up
+    return np.sum(residuals * residuals)
+
+
+def gds_form(feature, y_values, step_size: float = 0.01, tolerance: float = 0.05, max_iter: int = 100):
+    slope, intercept = ols_closed_form(feature, y_values)
+    magnitude = np.inf
+    iteration = 0
+    while magnitude > tolerance and iteration < max_iter:
+        # Compute the predicted values given the current slope and intercept
+        predictions = regression_prediction(feature, intercept, slope)
+
+        # Compute the prediction errors (prediction - Y)
+        residual_errors = predictions - y_values
+
+        # Update the intercept
+        # The derivative of the cost for the intercept
+        # is the sum of the errors
+        intercept -= step_size * np.sum(residual_errors)
+
+        # Update the slope
+        # The derivative of the cost for the slope
+        # is the sum of the product of the errors and the input
+        partial = np.sum(feature * residual_errors)
+        slope -= step_size * partial
+
+        # Compute the magnitude of the gradient
+        magnitude = np.sqrt(sum(residual_errors ** 2, partial ** 2))
+        iteration += 1
+
+    return slope, intercept
+
+
+if __name__ == "__main__":
+    feature_data = np.array([0, 1, 2, 3])
+    y_data = np.array([-1, 0.2, 0.9, 2.1])
+    # feature_data = np.array([0, 1, 2, 3, 4])
+    # y_data = np.array([1, 3, 7, 13, 21])
+    # feature_data = np.array([0, 1, 2, 3, 4])
+    # y_data = np.array([1, 2, 3, 4, 5])
+
+    m, c = ols_numpy(feature_data, y_data)
+    print(f"numpy based: ({m:.2f}, {c:.2f})")
+
+    m, c = ols_sklearn(feature_data, y_data)
+    print(f"sklearn based: ({m:.2f}, {c:.2f})")
+
+    m, c = ols_closed_form(feature_data, y_data)
+    print(f"closed form: ({m:.2f}, {c:.2f})")
+
+    m, c = gds_form(feature_data, y_data)
+    print(f"gds_form: ({m:.2f}, {c:.2f})")
+
+    plot_line(feature_data, y_data, m, c)
